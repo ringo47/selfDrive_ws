8b519c32620bbf116b07752e047786ce8bc80ef5
Varshith Solipuram <vsolipuram5@gmail.com>
2020-02-07 14:43:30 -0500

    Cmake

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ringo/selfDrive_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ringo/selfDrive_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/ringo/selfDrive_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/ringo/selfDrive_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/ringo/selfDrive_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/ringo/selfDrive_ws/src:$ROS_PACKAGE_PATH"
