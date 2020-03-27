93e7b8afec2a4178becdf2f42264b97b7e0c1b94
Varshith Solipuram <vsolipuram5@gmail.com>
2020-03-26 22:18:46 -0400

    Raspberry Pi E-Stop + Launch file with multiplots

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/ringo/selfDrive_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/ringo/vision_master/devel;/opt/ros/melodic".split(';') if "/home/ringo/vision_master/devel;/opt/ros/melodic" != "" else []
+underlay_workspaces = "/home/ringo/selfDrive_ws/devel;/home/ringo/vision_master/devel;/opt/ros/melodic".split(';') if "/home/ringo/selfDrive_ws/devel;/home/ringo/vision_master/devel;/opt/ros/melodic" != "" else []
--- devel/share/velodyne_driver/cmake/velodyne_driverConfig.cmake
+++ devel/share/velodyne_driver/cmake/velodyne_driverConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ringo/selfDrive_ws/devel/lib;/home/ringo/vision_master/devel/lib;/opt/ros/melodic/lib)
+    foreach(path /home/ringo/selfDrive_ws/devel/lib;/home/ringo/selfDrive_ws/devel/lib;/home/ringo/vision_master/devel/lib;/opt/ros/melodic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
